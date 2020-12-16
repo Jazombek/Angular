9bd1941a061a65760378b87049c127a644db7a25
Jazombek <37402899+Jazombek@users.noreply.github.com>
2020-12-16 09:54:14 +0100

    Tutorial ✓

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
       HeroesComponent,
       HeroDetailComponent,
       MessagesComponent,
-      DashboardComponent
+      DashboardComponent,
+      HeroSearchComponent
    ],
    imports: [
     BrowserModule,
